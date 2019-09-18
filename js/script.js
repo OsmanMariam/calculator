1aee825d9e5dd116b76865f942adc9f07c016b76
OsmanMariam <sufimariam8@gmail.com>
2019-09-18 15:46:13 +0300

    add index.html and readme file, js and css folders

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function add (number1,number2){
+    return number1 + number2;
+    }
+    var number1=parseInt(prompt("Enter a numnber:"));
+    var number2=parseInt(prompt("Enter another number:"));
+    var result=add(number1,number2);
+    alert(result);
